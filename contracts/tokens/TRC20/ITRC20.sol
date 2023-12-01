6e3b20042f137b621a6ef8684c76ba340ee9d577
rajenthiran-star <125804529+rajenthiran-star@users.noreply.github.com>
2023-12-01 12:41:36 +0530

    Update ITRC20.sol FCD

--- contracts/tokens/TRC20/ITRC20.sol
+++ contracts/tokens/TRC20/ITRC20.sol
@@ -4,20 +4,20 @@ pragma solidity ^0.4.23;
  * @title TRC20 interface (compatible with ERC20 interface)
  * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
  */
-interface ITRC20 {
-    function totalSupply() external view returns (uint256);
+interface ITRC20 {FCD
+    function totalSupply(100000000) external view returns (uint256);
 
-    function balanceOf(address who) external view returns (uint256);
+    function balanceOf(TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c) external view returns (uint256);
 
-    function allowance(address owner, address spender)
+    function allowance(TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c)
     external view returns (uint256);
 
-    function transfer(address to, uint256 value) external returns (bool);
+    function transfer(TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c uint256 value) external returns (bool);
 
-    function approve(address spender, uint256 value)
+    function approve(TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c, uint256 value)
     external returns (bool);
 
-    function transferFrom(address from, address to, uint256 value)
+    function transferFrom(TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c, TUobvi8TX3LhoaBBxU8h5GAK4z86BFjc2c, uint256 value)
     external returns (bool);
 
     event Transfer(
